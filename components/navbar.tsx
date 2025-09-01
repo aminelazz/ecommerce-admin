70e689302186323f3f2c5308c67f91452703331e
aminelazz <simo.20052011@gmail.com>
2025-09-01 22:43:25 +0100

    feat(theme): add theme toggle and provider for dark/light mode support

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,7 @@ import { redirect } from 'next/navigation'
 import MainNav from '@/components/main-nav'
 import StoreSwitcher from '@/components/store-switcher'
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from './theme-toggle'
 
 async function Navbar() {
   const { userId } = await auth();
@@ -26,6 +27,7 @@ async function Navbar() {
         <StoreSwitcher items={stores} />
         <MainNav className='mx-6' />
         <div className='ml-auto flex items-center space-x-4'>
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
